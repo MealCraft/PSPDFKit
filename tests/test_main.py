53f224194dc0103554d3db3b2918483975608865
Jun-te Kim <junte.kim@mealcraft.com>
2024-09-05 05:25:22 +0000

    added my first test files

---
+++ tests/test_main.py
@@ -0,0 +1,18 @@
+import pytest
+from http import HTTPStatus
+
+
+def test_it_returns_correct_message_on_index_page(app):
+    with app.test_client() as c:
+        response = c.get(f"/", follow_redirects=True)
+        assert response.status_code == HTTPStatus.OK
+
+    expected_msg = "Hello from PSPDFKit Engineer"
+    assert response.text == expected_msg, \
+        f"Did not return correct welcome message. Got {response.text}, expected: {expected_msg}"
+
+
+def test_health_status_for_k8(app):
+    with app.test_client() as c:
+        response = c.get(f"/health", follow_redirects=True)
+        assert response.status_code == HTTPStatus.OK
