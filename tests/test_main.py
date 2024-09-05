4b35ad00889c4ccdce092aeb3f7b1cc31d8bf150
Jun-te Kim <junte.kim@mealcraft.com>
2024-09-05 06:37:28 +0000

    line too long file

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,19 +5,23 @@ def test_it_returns_correct_message_on_index_page(app):
     with app.test_client() as c:
         response = c.get("/", follow_redirects=True)
         if response.status_code != HTTPStatus.OK:
-            raise AssertionError(f"Expected HTTPStatus.OK, got 
-                                 {response.status_code}")
-
+            raise AssertionError(
+                f"Expected HTTPStatus.OK, got {response.status_code}"
+            )
 
         expected_msg = "Hello from PSPDFKit Engineer"
         if response.text != expected_msg:
-            raise AssertionError(f"Did not return correct welcome message.
-                                  Got {response.text}, expected: {expected_msg}")
+            raise AssertionError(
+                f"Did not return correct welcome message. Got {response.text},"
+                f" expected: {expected_msg}"
+            )
 
 
 def test_health_status_for_k8(app):
     with app.test_client() as c:
         response = c.get("/health", follow_redirects=True)
         if response.status_code != HTTPStatus.OK:
-            raise AssertionError(f"Expected HTTPStatus.OK for health check,
-                                  got {response.status_code}")
+            raise AssertionError(
+                "Expected HTTPStatus.OK for health check, "
+                f"got {response.status_code}"
+            )
